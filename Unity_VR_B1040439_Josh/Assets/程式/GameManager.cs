16fee947ce2da1e1b42fc8428c1e769e09be9501
rio19991229 <55218512+rio19991229@users.noreply.github.com>
2020-01-06 21:48:49 +0800

    end

---
+++ Unity_VR_B1040439_Josh/Assets/程式/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public void ThrowPick()
+    {
+
+    }
+    public void ThrowDetach(GameObject obj)
+    {
+        Destroy(obj);
+        Score.ballscore += 1;
+    }
+    public void ThrowHeld()
+    {
+        
+    }
+}
---
+++ Unity_VR_B1040439_Josh/Assets/程式/Score.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public static int ballscore;
+    public GameObject all;
+    public GameObject text;
+
+    void Update()
+    {
+        if (ballscore >= 7)
+        {
+            Destroy(all);
+            Destroy(text);
+        }
+    }
+}
